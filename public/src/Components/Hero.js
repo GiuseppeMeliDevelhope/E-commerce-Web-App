1b9eeb42c50c898a22a7c5838e54b62ffcfbae75
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-10-17 14:16:01 +0200

    added productdetails by clicking eye icon

--- public/src/Components/Hero.js
+++ public/src/Components/Hero.js
@@ -9,7 +9,7 @@ function Hero () {
     
     return(
         
-        <section className="bg-blue-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24 ">
+        <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24 ">
           <div className="container mx-auto flex justify-around h-full">
              {/* text */}
              <div className="flex flex-col justify-center">
